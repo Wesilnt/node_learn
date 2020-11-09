7f7291947dffd301faf027d5ee39a1c37a1e1ea6
lei.wu <lei.wu@mobilemd.cn>
2020-11-09 15:42:02 +0800

    更新order

--- order.js
+++ order.js
@@ -2,21 +2,23 @@ const order = require("./order/order");
 
 const rememberMe = false;
 const password_d = "Taimei@123";
+const phone_d = "15201780237";
 const user_d = "lei.wu";
 
 const getUser = () => {
+  
   const paramArr = process.argv.splice(2);
-  let username = paramArr[0] || user_d;
-  let password = password_d;
-  if (paramArr.length > 1) {
-    password = paramArr[1];
-  }
 
-  console.log(`订餐者 : ${username}`);
+  // console.log(paramArr)
+
+  const [username=user_d,phone=phone_d,password=password_d] = paramArr
+
+  console.log(`${username} , 订餐电话：${phone} `);
 
   return {
     username,
     password,
+    phone,
     rememberMe,
   };
 };
--- order/order.js
+++ order/order.js
@@ -2,25 +2,34 @@ const fetch = require("node-fetch");
 const querystring = require("querystring");
 const { url } = require("./common.js");
 const login = require("./login");
+const FormData = require('form-data');
 
 module.exports = async function (params) {
-  const cookie = await login(params);
-  if (cookie === false) throw new Error("login fial");
+  const {phone,...loginInfo} = params;
+  const cookie = await login(loginInfo);
+  if (cookie === false) throw new Error("login fail");
+
+  const form = new FormData();
+
+  form.append('city', '上海');
+  form.append('phone', phone);
 
   const ordering = await fetch(
-    `${url}module/dinner/add?${querystring.stringify({
-      city: "上海",
-      remark: "test node 命令行 订餐",
-    })}`,
+    `${url}module/dinner/add`,
     {
       method: "post",
       headers: {
-        "Content-Type": "application/json;charset=UTF-8",
+        // "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
         Cookie: cookie,
+        ...form.getHeaders()
       },
+      body: form
     }
   ).then((res) => {
-    return res.json();
+    return res.json({
+      city: "上海",
+      remark: "test node 命令行 订餐",
+    });
   });
   return ordering;
 };
